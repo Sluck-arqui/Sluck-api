9645cc36f2d7f2a7a6babfd89c84f5c61e26c602
nachocontreras <icontreras1@uc.cl>
2018-10-06 20:34:51 -0300

    POST & DELETE requests

--- sluck_api/urls.py
+++ sluck_api/urls.py
@@ -1,19 +1,22 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from sluck_api import views
+from django.views.decorators.csrf import csrf_exempt
 
 # router = routers.DefaultRouter()
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^message/$', views.get_message, name='get_message'),
-    url(r'^user/$', views.get_user, name='get_user'),
-    url(r'^group/$', views.get_group, name='get_group'),
-    url(r'^message/group/$', views.post_message, name='post_message'),
+    url(r'^message/$', csrf_exempt(views.get_message), name='get_message'),
+    url(r'^user/$', csrf_exempt(views.get_user), name='get_user'),
+    url(r'^group/$', csrf_exempt(views.get_group), name='get_group'),
+    url(r'^group/new/$', csrf_exempt(views.new_group), name='new_group'),
+    url(r'^group/member/$', csrf_exempt(views.group_member), name='group_member'),
+    url(r'^message/group/$', csrf_exempt(views.post_message), name='post_message'),
     url(r'^messages/group/$', views.get_group_messages, name='get_group_messages'),
-    url(r'^message/like/$', views.like_message, name='like_message'),
-    url(r'^message/dislike/$', views.dislike_message, name='dislike_message'),
+    url(r'^message/like/$', csrf_exempt(views.like_message), name='like_message'),
+    url(r'^message/dislike/$', csrf_exempt(views.dislike_message), name='dislike_message'),
     url(r'^message/reactions/$', views.get_reactions, name='message_reactions'),
     url(r'^search/hashtag/$', views.search_hashtag, name='search_hashtag'),
     url(r'^search/username/$', views.search_username, name='search_username')
--- sluck_api/views.py
+++ sluck_api/views.py
@@ -1,65 +1,116 @@
 from sluck_api.serializers import user_serializer, message_serializer, group_serializer
 from django.http.response import JsonResponse
-from sluck_api.models import Message, User, Group, MessageHashtag, Hashtag
+from sluck_api.models import Message, User, Group, MessageHashtag, Hashtag, \
+                            MessageLike, MessageDislike, UserGroup
 
 
 # Por ahora http://127.0.0.1:8000/user/?user_id=1
 # requests.get(url, params={'user_id':1})
 def get_user(request):
-    try:
-        user_id = request.GET.get('user_id')[0]
-        users = User.objects.filter(id=user_id)
-        user = users[0]
-    except (IndexError, TypeError):
-        return JsonResponse({'status_code': 404, 'status_text': 'Object user not found.'}, 
-        json_dumps_params={'indent': 2})
-    return JsonResponse({'status_code': 200, 'status_text':'Ok', 'user': user_serializer(user)},
-        json_dumps_params={'indent': 2})
+    if request.method == 'GET':
+        try:
+            user_id = request.GET.get('user_id')[0]
+            users = User.objects.filter(id=user_id)
+            user = users[0]
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object user not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Ok', 'user': user_serializer(user)},
+            json_dumps_params={'indent': 2})
+    elif request.method == 'DELETE':
+        try:
+            user_id = request.GET.get('user_id')[0]
+            users = User.objects.filter(id=user_id)
+            user = users[0]
+            user.delete()
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object user not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Deleted successfully'},
+            json_dumps_params={'indent': 2})
+    else:
+        return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                            json_dumps_params={'indent': 2})
+
 
 # Por ahora http://127.0.0.1:8000/message/?message_id=1
 # requests.get(url, params={'message_id':1})
 def get_message(request):
-    try:
-        message_id = request.GET.get('message_id')[0]
-        messages = Message.objects.filter(id=message_id)
-        message = messages[0]
-    except (IndexError, TypeError):
-        return JsonResponse({'status_code': 404, 'status_text': 'Object message not found.'}, 
-        json_dumps_params={'indent': 2})
-    return JsonResponse({'status_code': 200, 'status_text':'Ok', 'message': message_serializer(message)},
-        json_dumps_params={'indent': 2})
+    if request.method == 'GET':
+        try:
+            message_id = request.GET.get('message_id')[0]
+            messages = Message.objects.filter(id=message_id)
+            message = messages[0]
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object message not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Ok', 'message': message_serializer(message)},
+            json_dumps_params={'indent': 2})
+    elif request.method == 'DELETE':
+        # requests.delete(url, params={'message_id':5})
+        try:
+            message_id = request.GET.get('message_id')[0]
+            messages = Message.objects.filter(id=message_id)
+            message = messages[0]
+            message.delete()
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object message not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Deleted successfully'},
+            json_dumps_params={'indent': 2})
+    else:
+        return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                            json_dumps_params={'indent': 2})
+
 
 # http://127.0.0.1:8000/group/?group_id=1
 # requests.get(url, params={'group_id':1})
 def get_group(request):
-    try:
-        group_id = request.GET.get('group_id')[0]
-        groups = Group.objects.filter(id=group_id)
-        group = groups[0]
-    except (IndexError, TypeError):
-        return JsonResponse({'status_code': 404, 'status_text': 'Object group not found.'}, 
-        json_dumps_params={'indent': 2})
-    return JsonResponse({'status_code': 200, 'status_text':'Ok', 'group': group_serializer(group)},
-        json_dumps_params={'indent': 2})
-
-
-def post_message(request):
-    # No esta funcionando por errores de crsf
-    if request.method == 'POST':
+    if request.method == 'GET':
         try:
-            group_id = request.POST.get('group_id')[0]
-            text = request.POST.get('text')
-            results = Group.objects.filter(id=group_id)
-            group = results[0]
-            new_message = Message(group_id=group_id, text=text, user_id=1) 
-            # Por mientras usuario 1, luego se revisa con los headers y las keys
-            new_message.publish()
-            print(new_message, new_message.id)
+            group_id = request.GET.get('group_id')[0]
+            groups = Group.objects.filter(id=group_id)
+            group = groups[0]
         except (IndexError, TypeError):
             return JsonResponse({'status_code': 404, 'status_text': 'Object group not found.'}, 
             json_dumps_params={'indent': 2})
-        return JsonResponse({'status_code': 201, 'status_text':'Created successfully', 'message': message_serializer(message)},
-        json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Ok', 'group': group_serializer(group)},
+            json_dumps_params={'indent': 2})
+    elif request.method == 'DELETE':
+        try:
+            group_id = request.GET.get('group_id')[0]
+            groups = Group.objects.filter(id=group_id)
+            group = groups[0]
+            group.delete()
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object group not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Deleted successfully'},
+            json_dumps_params={'indent': 2})
+    else:
+        return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                            json_dumps_params={'indent': 2})
+
+
+# requests.post(url, params={'group_id':1, 'text':'Estoy feliz #Happy #VivaLaVida'})
+def post_message(request):
+    if request.method == 'POST':
+        try:
+            group_id = request.GET.get('group_id')[0]
+            text = request.GET.get('text')
+            results = Group.objects.filter(id=group_id)
+            group = results[0]
+            new_message = Message(group_id=group_id, text=text, user_id=2) 
+            # Por mientras usuario 1, luego se revisa con los headers y las keys
+            new_message.publish()
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object group not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 201, 'status_text':'Created successfully', 
+            'message': message_serializer(new_message)}, json_dumps_params={'indent': 2})
+    else:
+        return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                            json_dumps_params={'indent': 2})
 
 
 # http://127.0.0.1:8000/messages/group/?group_id=1
@@ -81,11 +132,11 @@ def get_group_messages(request):
         json_dumps_params={'indent': 2})
 
 
+# requests.post(url, params={'message_id':3})
 def like_message(request):
-    # No esta funcionando por errores de crsf
     if request.method == 'POST':
         try:
-            message_id = request.POST.get('message_id')[0]
+            message_id = request.GET.get('message_id')[0]
             results = Message.objects.filter(id=message_id)
             message = results[0]
             new_like = MessageLike(message_id=message_id, user_id=1) 
@@ -96,16 +147,18 @@ def like_message(request):
             json_dumps_params={'indent': 2})
         return JsonResponse({'status_code': 201, 'status_text':'Created successfully', 'message': message_serializer(message)},
         json_dumps_params={'indent': 2})
+    return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                       json_dumps_params={'indent': 2})
 
 
+# requests.post(url, params={'message_id':3})
 def dislike_message(request):
-    # No esta funcionando por errores de crsf
     if request.method == 'POST':
         try:
-            message_id = request.POST.get('message_id')[0]
+            message_id = request.GET.get('message_id')[0]
             results = Message.objects.filter(id=message_id)
             message = results[0]
-            new_like = MessageDisLike(message_id=message_id, user_id=1) 
+            new_like = MessageDislike(message_id=message_id, user_id=1) 
             # Por mientras usuario 1, luego se revisa con los headers y las keys
             new_like.publish()
         except (IndexError, TypeError):
@@ -113,6 +166,8 @@ def dislike_message(request):
             json_dumps_params={'indent': 2})
         return JsonResponse({'status_code': 201, 'status_text':'Created successfully', 'message': message_serializer(message)},
         json_dumps_params={'indent': 2})
+    return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                        json_dumps_params={'indent': 2})
 
 
 # requests.get(url, params={'message_id':1})
@@ -161,6 +216,7 @@ def search_hashtag(request):
         'messages': messages},
         json_dumps_params={'indent': 2})
 
+
 # http://127.0.0.1:8000/search/username/?username=nachocontreras
 # # requests.get(url, params={'username': 'nachocontreras'})
 def search_username(request):
@@ -186,3 +242,55 @@ def search_username(request):
     return JsonResponse({'status_code': 200, 'status_text':'Ok', 
         'messages': messages},
         json_dumps_params={'indent': 2})
+
+
+# requests.post(url, params={'name':'Second group', 'description':'This is the second group'})
+def new_group(request):
+    if request.method == 'POST':
+        try:
+            name = request.GET.get('name')
+            description = request.GET.get('description')
+            group = Group(name=name, description=description)
+            group.publish()
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 400, 'status_text': 'Bad request.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 201, 'status_text':'Created successfully', 
+            'group': group_serializer(group)}, json_dumps_params={'indent': 2})
+    else:
+        return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                            json_dumps_params={'indent': 2})
+
+
+# requests.post(url, params={'group_id':3, 'user_id':1})
+def group_member(request):
+    if request.method == 'POST':
+        try:
+            group_id = request.GET.get('group_id')[0]
+            user_id = request.GET.get('user_id')[0]
+            groups = Group.objects.filter(id=group_id)
+            group = groups[0]
+            new_member = UserGroup(user_id=user_id, group_id=group_id)
+            new_member.publish()
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object group not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Added successfully', 
+            'group': group_serializer(group)}, json_dumps_params={'indent': 2})
+    elif request.method == 'DELETE':
+        # requests.delete(url, params={'group_id':3, 'user_id':1})
+        try:
+            group_id = request.GET.get('group_id')[0]
+            user_id = request.GET.get('user_id')[0]
+            groups = Group.objects.filter(id=group_id)
+            group = groups[0]
+            new_member = UserGroup.objects.filter(user_id=user_id, group_id=group_id)
+            new_member.delete()
+        except (IndexError, TypeError):
+            return JsonResponse({'status_code': 404, 'status_text': 'Object group or user not found.'}, 
+            json_dumps_params={'indent': 2})
+        return JsonResponse({'status_code': 200, 'status_text':'Deleted successfully'}, 
+                            json_dumps_params={'indent': 2})
+    else:
+        return JsonResponse({'status_code': 405, 'status_text': 'Method not allowed.'}, 
+                            json_dumps_params={'indent': 2})
