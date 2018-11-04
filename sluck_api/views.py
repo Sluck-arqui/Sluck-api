0dc75986f653cc14bf78877872d9dfd2dbe6631f
nachocontreras <icontreras1@uc.cl>
2018-11-04 19:13:47 -0300

    Added register try/except

--- sluck_api/views.py
+++ sluck_api/views.py
@@ -81,7 +81,10 @@ class ThreadMessageViewSet(viewsets.ModelViewSet):
 def register(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = UserSerializer(data=data)
+        try:
+            serializer = UserSerializer(data=data)
+        except:
+            return JsonResponse(STATUS_CODE_400, status=400)
         if serializer.is_valid():
             instance, token = serializer.save()
             data = serializer.data
